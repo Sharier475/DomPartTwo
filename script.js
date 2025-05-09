e370e1b7dc9bc044534c70f537948aa63c5f46a5
SHARIER ISLAM <Sharierpulok475@gmail.com>
2025-05-09 21:05:00 +0600

    Update index.html to include a list in the div and enhance script.js with dynamic element creation; adjust box height in style.css for improved layout

--- script.js
+++ script.js
@@ -24,20 +24,34 @@
 
     let para = document.querySelector("p");
     console.log(para.setAttribute("class", "JavaScriptDiv"));
-    
+
 }
 
 
 
 //Style
 {
-    let div = document.querySelector("div");
-    div.style.backgroundColor = "red";
-    div.innerText = "Hello!";
-    div.style.fontSize = "20px";
+    // let div = document.querySelector("div");
+    // div.style.backgroundColor = "red";
+    // div.innerText = "Hello!";
+    // div.style.fontSize = "20px";
 }
 
+//Create Elements / Add Elements
+{
+    let newBtn = document.createElement("button");
+    newBtn.innerText = "Click Me!";
+    console.log(newBtn);
+
+    let div = document.querySelector("div");
+    div.append(newBtn); // (adds at the last of the node.)
+    // div.prepend(newBtn);  //(adds at the start of the node.)
+    // div.before(newBtn);  // (adds before the node.)
+    // div.after(newBtn);   // (adds after the node.)
 
 
+    let newHeading = document.createElement("h1");
+    newHeading.innerHTML = "<i> Sharier Islam </i>";
 
-
+    document.querySelector("body").prepend(newHeading);
+}
