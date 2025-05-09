50010e4f959ef8ca2a43ba605afea8abd418df0e
SHARIER ISLAM <Sharierpulok475@gmail.com>
2025-05-09 20:16:58 +0600

    Refactor code for better readability and consistency in DOM manipulation

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 //Dom Manupulation (Attributes) 
 // GET
 {
-let div = document.querySelector("div");
-console.log(div);
+    let div = document.querySelector("div");
+    console.log(div);
 
-let id = div.getAttribute("id");
-console.log(id);
+    let id = div.getAttribute("id");
+    console.log(id);
 
-let name = div.getAttribute("name");
-console.log(name);
+    let name = div.getAttribute("name");
+    console.log(name);
 
 
-let para = document.querySelector("p");
-console.log(para.getAttribute("class"));
+    let para = document.querySelector("p");
+    console.log(para.getAttribute("class"));
 }
 
 
 
 // SET/Edit
+{
+    let div = document.querySelector("div");
+    console.log(div.setAttribute("name", "JavaScriptDiv"));
 
-let div = document.querySelector("div");
-console.log(div.setAttribute("name","JavaScriptDiv"));
+    let para = document.querySelector("p");
+    console.log(para.setAttribute("class", "JavaScriptDiv"));
+    
+}
 
-let para = document.querySelector("p");
-console.log(para.setAttribute("class","JavaScriptDiv"));
 
 
 //Style
+{
+    let div = document.querySelector("div");
+    div.style.backgroundColor = "red";
+    div.innerText = "Hello!";
+    div.style.fontSize = "20px";
+}
 
-div.style.backgroundColor = "red";
-div.innerText = "Hello!";
-div.style.fontSize = "20px";
+
+
+
+
